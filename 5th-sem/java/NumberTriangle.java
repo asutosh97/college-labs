91baf3bcd06c5a178846e384aad9e934456e33ec
Asutosh Sahoo <asutoshsahoo786@gmail.com>
2017-08-08 23:42:18 +0530

    NumberTriangle Output Indentation Correction

--- 5th-sem/java/NumberTriangle.java
+++ 5th-sem/java/NumberTriangle.java
@@ -8,12 +8,10 @@ class NumberTriangle
 
 		for(int i = 1; i <= n; i++)
 		{
-			/*
-			for(int j = 1; j <= (n - i)/2; j++)
+			for(int j = 1; j <= (n - i); j++)
 			{
 				System.out.print(" ");
 			}
-			*/
 			
 			for(int j = 1; j <= i; j++)
 			{
